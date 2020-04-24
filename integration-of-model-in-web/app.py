0643a432fa67b0278ed983b286985d1aa63af31f
mrpandey1 <pandeynishant12345@gmail.com>
2020-04-24 21:42:01 +0530

    added fruits/vegetables classifier

--- integration-of-model-in-web/app.py
+++ integration-of-model-in-web/app.py
@@ -112,8 +112,8 @@ def upload():
         # Save the file to ./uploads
         basepath = os.path.dirname(__file__)
         file_path = os.path.join(
-            basepath, 'uploads', secure_filename(f.filename))
-        #f.save(file_path)
+                basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
         # Make prediction
         result = model_predict(file_path,loaded_full_model)            
         return result[0]
